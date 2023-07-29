203e566c2e366abd26aa249386907eb49440a045
Jose Lopez Renteria <joselukas24@gmail.com>
2023-07-29 00:38:03 -0500

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <SelectedContact selectedContactId={selectedContactId} />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   );
 }
